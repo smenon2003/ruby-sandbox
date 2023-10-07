0fb9a485b0530d3f662399c036fe5f8eac4ccdb5
smenon2003 <146893219+smenon2003@users.noreply.github.com>
2023-10-07 19:24:15 +0000

    initial commit

---
+++ debugger.rb
@@ -0,0 +1,12 @@
+require "pry-byebug"
+
+lucky_num = rand(1..20)
+
+
+first_part =  "Your lucky number for today is " 
+
+last_part = "!"
+
+byebug
+
+pp first_part + lucky_num.to_s + last_part
---
+++ howdy.rb
@@ -0,0 +1,9 @@
+require "./goodbye.rb"
+require "active_support/all"
+
+# my_string = "Howdy there!"
+# pp my_string
+
+x = 1
+
+pp x.ordinalize
